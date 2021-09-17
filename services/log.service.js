ee2924b40938dcadb4df994bce8c0297394b7e15
Alaricode <antonlarichev@gmail.com>
2021-09-17 14:13:11 +0300

    Упражнение - Завершение приложения

--- services/log.service.js
+++ services/log.service.js
@@ -20,4 +20,15 @@ const printHelp = () => {
 	);
 };
 
-export { printError, printSuccess, printHelp };
+const printWeather = (res, icon) => {
+	console.log(
+		dedent`${chalk.bgYellow(' WEATHER ')} Погода в городе ${res.name}
+		${icon}  ${res.weather[0].description}
+		Температура: ${res.main.temp} (ощущается как ${res.main.feels_like})
+		Влажность: ${res.main.humidity}%
+		Скорость ветра: ${res.wind.speed}
+		`
+	);
+};
+
+export { printError, printSuccess, printHelp, printWeather };
--- weather.js
+++ weather.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
-import { getWeather } from './services/api.service.js';
-import { printHelp, printSuccess, printError } from './services/log.service.js';
-import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+import { getWeather, getIcon } from './services/api.service.js';
+import { printHelp, printSuccess, printError, printWeather } from './services/log.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY, getKeyValue } from './services/storage.service.js';
 
 const saveToken = async (token) => {
 	if (!token.length) {
@@ -17,10 +17,24 @@ const saveToken = async (token) => {
 	}
 }
 
+const saveCity = async (city) => {
+	if (!city.length) {
+		printError('Не передан город');
+		return;
+	}
+	try {
+		await saveKeyValue(TOKEN_DICTIONARY.city, city);
+		printSuccess('Город сохранён');
+	} catch (e) {
+		printError(e.message);
+	}
+}
+
 const getForcast = async () => {
 	try {
-		const weather = await getWeather(process.env.CITY);
-		console.log(weather); // Красивый вывод погоды
+		const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city);
+		const weather = await getWeather(city);
+		printWeather(weather, getIcon(weather.weather[0].icon));
 	} catch (e) {
 		if (e?.response?.status == 404) {
 			printError('Неверно указан город');
@@ -35,15 +49,15 @@ const getForcast = async () => {
 const initCLI = () => {
 	const args = getArgs(process.argv);
 	if (args.h) {
-		printHelp();
+		return printHelp();
 	}
 	if (args.s) {
-		// Сохранение города
+		return saveCity(args.s);
 	}
 	if (args.t) {
 		return saveToken(args.t);
 	}
-	getForcast();
+	return getForcast();
 };
 
 initCLI();
